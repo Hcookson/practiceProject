d0b7ebb233a84ebcc2b12d10e17524977a385d2d
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2019-01-11 19:06:36 -0500

    testing functionality of dice variable/declaring variables

---
+++ app.js
@@ -0,0 +1,14 @@
+// delcaring variables
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+// creating random function for rolling of dice
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+// grabbing current score in html and changing text depending on the number from the dice variable
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
