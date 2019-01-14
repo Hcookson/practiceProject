9c406bc9e53b52053172a9ede1ea9d5e5c2400dd
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2019-01-14 18:18:56 -0500

    testing functionality of simple javascript functions

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ console.log(dice);
 // grabbing current score in html and changing text depending on the number from the dice variable
 document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x =document.querySelector('#score-0').textContent;
+console.log(x);
+
+
+// setting starting image of dice to display none
+document.querySelector('.dice').style.display = 'none';
+
+// on click function
+function btn() {
+    // do something
+}
+btn();
+
+document.querySelector('.btn-roll').addEventListener('click', btn);
