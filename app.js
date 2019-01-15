3d880d6801621159cd9ddd327227f1069a6c5639
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2019-01-15 17:34:25 -0500

    creating dice functionality using ternary operators

--- app.js
+++ app.js
@@ -6,24 +6,42 @@ roundScore = 0;
 activePlayer = 1;
 
 // creating random function for rolling of dice
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-// grabbing current score in html and changing text depending on the number from the dice variable
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// // grabbing current score in html and changing text depending on the number from the dice variable
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x =document.querySelector('#score-0').textContent;
-console.log(x);
+// var x =document.querySelector('#score-0').textContent;
+// console.log(x);
 
 
 // setting starting image of dice to display none
 document.querySelector('.dice').style.display = 'none';
 
-// on click function
-function btn() {
-    // do something
-}
-btn();
+// setting scores on start of game
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
 
-document.querySelector('.btn-roll').addEventListener('click', btn);
+// on click function displaying dice images depending on number rolled
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'public/practiceProject/dice-' + dice + '.png';
+    // update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        // adding score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+       activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+       roundScore = 0;
+    }
+});
